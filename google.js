16c0936be5d2aaf6f04f095fb13e9699c3fe66d5
zdiggity <152721274+iheartgithub1234@users.noreply.github.com>
2024-01-13 08:26:24 -0500

    google.js

--- google.js
+++ google.js
@@ -1,4 +1,5 @@
-mainSearch = document.getElementById("mainSearch");
+let mainSearch = document.getElementById("mainSearch");
+let feelingArray = ["Im feeling lucky", "Im feeling adventerous", "Im feeling stellar", "Im feeling artistic", "Im feeling curious", "Im feeling trendy", "Im feeling doodly"];
 
 mainSearch.addEventListener("keydown", function(event) {
     if(event.key === "Enter") {
@@ -10,4 +11,8 @@ function search() {
     if(mainSearch.value != "") {
         location.replace("https://www.google.com/search?q=" + encodeURIComponent(mainSearch.value));
     }
+}
+
+function imFeelinLucky() {
+    location.replace("https://www.google.com/search?q=" + feelingArray[Math.floor(Math.random() * feelingArray.length)]);
 }
